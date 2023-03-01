43e5308e2fd4646836d6885e4eb8826b2d483b40
bayan-404 <bayan.jubah@gmail.com>
2023-03-01 09:21:51 +0200

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import App from './App'
+import Wishlist from './Wishlist';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <div>page not found</div>,
+  },
+  {
+    path:'/wishlist',
+    element: <Wishlist/>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
